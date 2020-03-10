762602a51840be2852a5e1833975f88e184de8a5
Alexey Krasnov <web.worddoc@gmail.com>
2020-03-10 23:17:56 +0300

    test(formikstate): add tests

---
+++ src/FormikState/FormikState.test.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import { Formik, FormikForm, FormikState } from './../';
+
+
+configure({ adapter: new Adapter() });
+
+const MockComponent = () => (
+    <></>
+)
+
+describe('FormikState', () => {
+    const component = mount(
+        <Formik
+            initialValues={{
+                testName: 'test value'
+            }}
+            onSubmit={jest.fn()}
+            render={() => (
+                <FormikForm render={() => (
+                    <FormikState render={props => (
+                        <MockComponent {...props} />
+                    )}/>
+                )}/>
+            )}
+        />
+    )
+    const props: any = component.find(MockComponent).props();
+
+    it('renders correctly', () => {
+        expect(component.find(MockComponent).exists()).toBe(true);
+    });
+
+    it('descends expected props', () => {
+        expect(props.initialValues).toEqual({ testName: 'test value' });
+        expect(props.errors).toEqual({});
+        expect(props.touched).toEqual({});
+        expect(props.validateOnBlur).toBe(false);
+        expect(props.validateOnChange).toBe(true);
+        expect(props.validateOnMount).toBeFalsy();
+        expect(props).toHaveProperty('status');
+        expect(props).toHaveProperty('isSubmitting');
+        expect(props).toHaveProperty('isValidating');
+        expect(props).toHaveProperty('submitCount');
+        expect(props).toHaveProperty('initialErrors');
+        expect(props).toHaveProperty('initialTouched');
+        expect(props).toHaveProperty('initialStatus');
+        expect(props).toHaveProperty('handleBlur');
+        expect(props).toHaveProperty('handleChange');
+        expect(props).toHaveProperty('handleReset');
+        expect(props).toHaveProperty('handleSubmit');
+        expect(props).toHaveProperty('resetForm');
+        expect(props).toHaveProperty('setErrors');
+        expect(props).toHaveProperty('setFormikState');
+        expect(props).toHaveProperty('setFieldTouched');
+        expect(props).toHaveProperty('setFieldValue');
+        expect(props).toHaveProperty('setFieldError');
+        expect(props).toHaveProperty('setStatus');
+        expect(props).toHaveProperty('setSubmitting');
+        expect(props).toHaveProperty('setTouched');
+        expect(props).toHaveProperty('setValues');
+        expect(props).toHaveProperty('submitForm');
+        expect(props).toHaveProperty('validateForm');
+        expect(props).toHaveProperty('validateField');
+        expect(props).toHaveProperty('isValid');
+        expect(props).toHaveProperty('dirty');
+        expect(props).toHaveProperty('unregisterField');
+        expect(props).toHaveProperty('registerField');
+        expect(props).toHaveProperty('getFieldProps');
+        expect(props).toHaveProperty('getFieldMeta');
+        expect(props).toHaveProperty('getFieldHelpers');
+    })
+})
